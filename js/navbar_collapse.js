9b0ce8dbddf6850d80bdec371f42e2ef652f5b1e
James W. Johnson <43220904+giganano@users.noreply.github.com>
2022-02-17 17:32:22 -0500

    style: navbar collapse button changes to X rather than changing color

--- js/navbar_collapse.js
+++ js/navbar_collapse.js
@@ -3,18 +3,49 @@
  * mobile screens.
  */ 
 
-/* Open or close the navbar when the collapse button is clicked */
+
+
+/* --------------------------------------------------------------------
+ * Open or close the navbar when the collapse button is clicked.
+ */
 $(document).ready(function() {
 	$(".toggle-collapse").click(function() {
 		$(".navbar-collapse").toggleClass("collapse")
 	})
 });
+/* -------------------------------------------------------------------- */
 
 
-/* Close it when one of the buttons inside the navbar is clicked */
+
+
+/* --------------------------------------------------------------------
+ * Close it when one of the buttons inside the navbar is clicked
+ */
 $(document).ready(function() {
 	$(".nav-item").click(function() {
 		$(".navbar-collapse").toggleClass("collapse")
 	})
 });
+/* -------------------------------------------------------------------- */
+
+
+
+
+/* --------------------------------------------------------------------
+ * 
+ * Swap the mobile navigation buttom from the align-center icon to an X
+ * when it's opened, and vice-versa when it's closed.
+ */
+$(document).ready(function() {
+	$(".toggle-collapse").click(function() {
+		$(".fa-solid").toggleClass("fa-align-center")
+	})
+});
+
+$(document).ready(function() {
+	$(".toggle-collapse").click(function() {
+		$(".fa-solid").toggleClass("fa-x")
+	})
+});
+/* -------------------------------------------------------------------- */
 
